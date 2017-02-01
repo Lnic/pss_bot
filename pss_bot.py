4b752f8121c80107243192decb6324ae962102c1
DT-1236 <wemascarinas@gmail.com>
2017-02-01 01:35:59 -0600

    Added stats functionality
    Also updated the commandlist

--- pss_bot.py
+++ pss_bot.py
@@ -27,17 +27,63 @@ def all_names(sheet): #Assembles all names in the sheet
 def add_to_prestiges(name_list, source_sheet): #Adds all values in that sheet to the Prestige dictionary
     for name1 in range(1,len(name_list)+1):
         for name2 in range(1,len(name_list)+1):
-            prestiges["%s_%s"%(name_list[name1-1],name_list[name2-1])]=source_sheet[name1][name2]
+            prestiges["%s_%s"%(name_list[name1-1],name_list[name2-1])]=source_sheet[name1][name2].lower()
 
 heroes = all_names(hero) #plural indicates name list
 epics = all_names(epic)
 uniques = all_names(unique)
 
 add_to_prestiges(heroes,hero)
+legendaries=list(set(prestiges.values()))
 add_to_prestiges(epics,epic)
 add_to_prestiges(uniques,unique)
 
-print ('Data prep complete. Time Elapsed :'+str(time.clock()-time_start))
+print ('Prestige data prep complete. Time Elapsed :'+str(time.clock()-time_start))
+
+def finder(target, source):
+    return source[source.find(target)+len(target)+1:source.find('"',source.find(target)+len(target)+2)]
+
+class Crew:
+    def __init__(self, source, name):
+        '''Crew from Pixel Starships'''
+        #self.name = finder("CharacterDesignName=",source)
+        self.name = name
+        self.gender = finder("GenderType=",source)
+        self.race = finder("RaceType=",source)
+        self.hp = finder("FinalHp=",source)
+        self.pilot = finder("FinalPilot=",source)
+        self.attack = finder("FinalAttack=",source)
+        self.fire_resistance = finder("FireResistance=",source)
+        self.repair = finder("FinalRepair=",source)
+        self.weapon = finder("FinalWeapon=",source)
+        self.shield = finder("FinalShield=",source)
+        self.engine = finder("FinalEngine=",source)
+        self.research = finder("FinalResearch=",source)
+        self.walking_speed = finder("WalkingSpeed=",source)
+        self.rarity = finder("Rarity=",source)
+        self.progression = finder("ProgressionType=",source)
+        self.xp = finder("XpRequirementScale=",source)
+        self.special_type = finder("SpecialAbilityType=",source)
+        self.special = finder("SpecialAbilityFinalArgument=",source)
+        self.training = finder("TrainingCapacity=",source)
+        self.equipment = finder("EquipmentMask=",source)
+
+crew={}
+
+def all_crew_values(source):
+    text=source[:]
+    while len(text)>0:
+        name=(str(finder("CharacterDesignName=",text)).lower())
+        crew[name]=Crew(text,str(name))
+        if text.find("/CharacterDesign")>0:
+            text=text[text.find("/CharacterDesign")+len("/CharacterDesign")+1:]
+        else:
+            break
+        
+pss_api=open("ListAllCharacterDesigns2.txt","r+").read()
+all_crew_values(pss_api)
+
+print ('Crew data prep complete. Time Elapsed :'+str(time.clock()-time_start))
 
 import discord
 from discord.ext import commands
@@ -55,16 +101,18 @@ async def on_ready():
     print(bot.user.id)
     print('------')
     await bot.change_presence(game=discord.Game(name='Try ?commandlist'))
-
+ 
 @bot.command(aliases=["CommandList", "Commandlist", "Command List", "command list", "commands", "Commands", "command", "Command"])
 async def commandlist(*, request : str):
     try:
-        if request=="prestige" or request=="prestiges":
+        if request.lower()=="prestige" or request.lower()=="prestiges":
             await bot.say("Input: 1 crew name or 2 crew names separated by a comma. Gives result of crew prestige if known. Gives known possibilities for prestige if only 1 name is given")
-        elif request=="namelist" or request=="names":
+        elif request.lower()=="namelist" or request.lower()=="names":
             await bot.say("Gives acceptable spelling for Hero, Epic, or Unique crew")
-        elif request=="recipe" or request=="recipes":
+        elif request.lower()=="recipe" or request.lower()=="recipes":
             await bot.say("Gives known combinations which result in the given name. If 1 parent and 1 child are given, a list of other parents will be given which can produce the desired child.")
+        elif request.lower()=="stats" or request.lower()=="stat":
+            await bot.say("Gives requested stats for listed crew member. Use ?stats help for more information")
     except:
         await bot.say("I don't know what that is")
 
@@ -78,8 +126,8 @@ async def recipe(*,request : str):
         try:
             result=[]
             for combo in prestiges:
-                if request.title() in prestiges[combo]:
-                    result.append([combo.title().split('_')][0])
+                if request.lower() in prestiges[combo]:
+                    result.append([combo.lower().split('_')][0])
             if result==[]:
                 await bot.say("There are no recorded recipes for %s"%(request.title()))
                 return
@@ -92,9 +140,9 @@ async def recipe(*,request : str):
     try:
         result=[]
         for combo in prestiges:
-            if names[0].title() in prestiges[combo]: #names[0] is the child here
-                result.append([combo.title().split('_')][0])
-        if not [x for x in result if names[1].title() in x]:
+            if names[0].lower() in prestiges[combo]: #names[0] is the child here
+                result.append([combo.lower().split('_')][0])
+        if not [x for x in result if names[1].lower() in x]:
             for combo in prestiges:
                 if names[1].title() in prestiges[combo]: #names[1] is the child here
                     result.append([combo.title().split('_')][0])
@@ -123,7 +171,10 @@ async def recipe(*,request : str):
 @bot.command(aliases=["names"])
 async def namelist(request : str):
     """returns a list of valid names"""
-    if request.lower()=='hero' or request.lower()=='heroes':
+    if request.lower()=='legendary' or request.lower()=='legendaries' or request.lower()=='legend':
+        await bot.say('The following is a list of all known names of Legendary Crew. I will likely only understand names that are typed as follows:')
+        await bot.say(legendaries)
+    elif request.lower()=='hero' or request.lower()=='heroes':
         await bot.say('The following is a list of all known names of Hero Crew. I will likely only understand names that are typed as follows:')
         await bot.say(heroes)
     elif request.lower()=='epic' or request.lower()=='epics':
@@ -152,7 +203,7 @@ async def prestige(*,request : str):
         elif request.lower() in uniques:
             result=(set([x for x in unique if x[0]==request.title()][0][1:]))
         else:
-            await bot.say("Unrecognized. Please check spelling with ?namelist")
+            await bot.say("Unrecognized or the crew is not hero, epic, or unique. Please check spelling with ?namelist")
             return
         if result==set():
             await bot.say("No known recipes including %s"%(request.title()))
@@ -176,4 +227,20 @@ async def prestige(*,request : str):
     except:
         await bot.say("Please separate names by a comma (,) and reference ?namelist for spelling. Don't mix rarities.")
 
+@bot.command(aliases=["Stats", 'stat', 'Stat'])
+async def stats(*,request : str):
+    if request.lower()=='help':
+        await bot.say("Give a crew name and a stat separated by a comma. Valid names can be found with ?stat names. Valid stats are ```gender, race, hp, pilot, attack, fire_resistance, repair, weapon, shield, engine, research, walking_speed, rarity, progression, xp, special_type, special, training, and equipment.```")
+        return
+    if request.lower()=='names':
+        await bot.say("Valid names are: ```%s```"%(crew.keys()))
+    if ',' in request:
+        request = request.lower().split(',')
+    if request[1][0] == ' ':
+        request[1] = request[1][1:]
+    try:
+        await bot.say("%s's %s is: %s"%(request[0].title(), request[1], getattr(crew[request[0]],request[1])))
+    except:
+        await bot.say("I don't understand something. Try ?help to correct spelling")
+  
 bot.run('Mjc0NDU1NTg5NTY2ODA4MDc2.C2yWMw.uFEcuSxiZRJPp-UkloTvWV0z84Q')
